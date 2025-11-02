a4e2ed9a15c7b627f8f13d79dd48c8c7c26228a1
Adishesh Iyengar <adisheshiyengar@Adisheshs-MacBook-Pro.local>
2025-11-02 01:17:35 -0500

    Add helper script

---
+++ open_github.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Open GitHub in browser
+open "https://github.com/new?name=data-quality-analyzer&description=Web+application+for+analyzing+product+catalog+data+quality+and+ML+readiness"
